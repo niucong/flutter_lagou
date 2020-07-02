d27e7d71c41fb92d9bf56ab5abe4c544999b9f04
牛聪 <niucong@julong.cc>
2020-07-02 22:47:52 +0800

    InheritedWidget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_lagou/main.dart';
+import 'package:two_you_friend/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
